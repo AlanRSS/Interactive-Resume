7e838dea48b594b98345260d57a12d3d59530436
Alan Santos <alanrss_1@hotmail.com>
2015-04-04 16:40:46 +0100

    Encapsulated methods inside objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 var bio = {
 "name" : "Alan Santos",
 "role" : "developer",
@@ -14,12 +6,26 @@ var bio = {
 "skills" : ["programing", "css","html","git","animation","windows server","linux","tribox", "n+", "SQL","C#","Adobe Creative suite"],
 "email" : "alanrss_1@hotmail.com",
 "location" : "Lubango, Angola",
-"image" : "images/Alan.jpg"
-};
+"image" : "images/Alan.jpg",
+"displaybio" : function displaybio(){
+var specialname = HTMLheaderName.replace("%data%", bio.name);
+var specialrole = HTMLheaderRole.replace("%data%", bio.role);
+var Picture= HTMLbioPic.replace("%data%", bio.image);
+var specialWelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+var specialContact = HTMLmobile.replace("%data%", bio.contact);
+var specialMail = HTMLemail.replace("%data%", bio.email);
+var specialLocation = HTMLlocation.replace("%data%", bio.location);
+$("#main").prepend(specialWelcomeMessage);
+$("#main").prepend(Picture+specialname+"<p></p>"+specialrole);
+$("#topContacts").append("<br>"+"<p>"+ specialMail+"</p><p> "+ specialContact+"</p><p>"+specialLocation+"</p>");
+$("#header").append(HTMLskillsStart);
+for(value in bio.skills) {
+var formattedSkill = HTMLskills.replace("%data%", bio.skills[value]);
+$("#skills").append(formattedSkill);
+}}};
 
 var education = {
-"schools": [
-	{
+"schools" : [{
 	"name": "Colegio 123",
 	"city": "Lubango ANG",
 	"degree": "High School",
@@ -39,64 +45,15 @@ var education = {
 	"degree": "BSC",
 	"major" :"CompEngi",
 	"year" : "2012-2014"
-	}
-	]
+	}]
 	,
-	"onlineCourses": [
-	{
+	"onlineCourses": [{
 	"name" : "Udacity",
 	"degree" : "Front End Web Developer",
 	"URL" : "www.udacity.com",
 	"year" : "2015"
-	}
-	]
-}	
-	
-var work = [
-	{
-	"employer" : "PSL",
-	"role" : "web designer and developer",
-	"year" : "2015",
-	"location" : "Lubango ANG",
-	"description" : " Development of logos, business cards and website for the company."
-	},
-	{
-	"employer" : "Farost",
-	"role" : "student",
-	"year" : "1998-2015",
-	"location" : "Lubango ANG, Johannesburg SA",
-	"description" : " Under the orientation of my parents I studied and built my academical curriculum"
-	}]
-
-var projects =[ {
-	"title" : "PSLLogo",
-	"year" : "2015",
-	"location" : "Luanda, ANG",
-	"description" : "logo development for a company that is being built in Angola, the development took several hours and the logo elemnts reference cultural and business related elements",
-	"image" : "images/logo.jpg"
-}]
-
-
-
- 
-function displaybio(){
-var specialname = HTMLheaderName.replace("%data%", bio.name);
-var specialrole = HTMLheaderRole.replace("%data%", bio.role);
-var Picture= HTMLbioPic.replace("%data%", bio.image);
-var specialWelcomeMessage = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
-var specialContact = HTMLmobile.replace("%data%", bio.contact);
-var specialMail = HTMLemail.replace("%data%", bio.email);
-var specialLocation = HTMLlocation.replace("%data%", bio.location);
-$("#main").prepend(specialWelcomeMessage);
-$("#main").prepend(Picture+specialname+"<p></p>"+specialrole);
-$("#header").append(HTMLskillsStart);
-for(value in bio.skills) {
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[value]);
-	$("#skills").append(formattedSkill);
-}
-$("#topContacts").append("<br>"+"<p>"+ specialMail+"</p><p> "+ specialContact+"</p><p>"+specialLocation+"</p>");
-}
-function displayEducation(){
+	}],
+	"displayEducation": function displayEducation(){
 for(value in education.schools) {
 $("#education").append(HTMLschoolStart);
 
@@ -109,28 +66,8 @@ $("#education").append(HTMLschoolStart);
 	$(".education-entry:last").append(formattedDate);
 	var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[value].major)
 	$(".education-entry:last").append(formattedMajor);
-
-	}
-}
-}
-function displayWork(){
-for(value in work) {
-$("#workExperience").append(HTMLworkStart);
-
-	if(work.hasOwnProperty(value)===true){
-		var formattedWork = HTMLworkEmployer.replace("%data%", work[value].employer)
-	$(".work-entry:last").append(formattedWork);
-	var formattedTitle = HTMLworkTitle.replace("%data%", work[value].role)
-	$(".work-entry:last").append(formattedTitle);
-	var formattedDate = HTMLworkDates.replace("%data%", work[value].year)
-	$(".work-entry:last").append(formattedDate);
-	var formattedDescription = HTMLworkDates.replace("%data%", work[value].description)
-	$(".work-entry:last").append(formattedDescription);
-
-	}
-}
-}
-function displayOnline(){
+}}},
+"displayOnline" :function displayOnline(){
 for(value in education.onlineCourses) {
 $(".education-entry:last").append(HTMLonlineClasses);
 
@@ -143,33 +80,64 @@ $(".education-entry:last").append(HTMLonlineClasses);
 	$(".education-entry:last").append(formattedDate);
 	var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[value].URL)
 	$(".education-entry:last").append(formattedURL);
+}}}}	
+	
+var work = {
+	"job" : [{
+	"employer" : "PSL",
+	"role" : "web designer and developer",
+	"year" : "2015",
+	"location" : "Lubango ANG",
+	"description" : " Development of logos, business cards and website for the company."
+	},
+	{
+	"employer" : "Farost",
+	"role" : "student",
+	"year" : "1998-2015",
+	"location" : "Lubango ANG, Johannesburg SA",
+	"description" : " Under the orientation of my parents I studied and built my academical curriculum"
+	}],
+	"displayWork":function displayWork(){
+	for(value in work.job) {
+	$("#workExperience").append(HTMLworkStart);
 
-	}
-}
-}
-function displayProjects(){
-	for(value in projects){
+	if(work.job.hasOwnProperty(value)===true){
+		var formattedWork = HTMLworkEmployer.replace("%data%", work.job[value].employer)
+	$(".work-entry:last").append(formattedWork);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.job[value].role)
+	$(".work-entry:last").append(formattedTitle);
+	var formattedDate = HTMLworkDates.replace("%data%", work.job[value].year)
+	$(".work-entry:last").append(formattedDate);
+	var formattedDescription = HTMLworkDates.replace("%data%", work.job[value].description)
+	$(".work-entry:last").append(formattedDescription);
+}}}}
+
+var projects ={
+	"project" : [{
+	"title" : "PSLLogo",
+	"year" : "2015",
+	"location" : "Luanda, ANG",
+	"description" : "logo development for a company that is being built in Angola, the development took several hours and the logo elemnts reference cultural and business related elements",
+	"image" : "images/logo.jpg"
+}],
+	"displayProjects": function displayProjects(){
+	for(value in projects.project){
 	$("#projects").append(HTMLprojectStart);
-		if(projects.hasOwnProperty(value)===true){
-			var formattedName = HTMLprojectTitle.replace("%data%", projects[value].title)
+		if(projects.project.hasOwnProperty(value)===true){
+			var formattedName = HTMLprojectTitle.replace("%data%", projects.project[value].title)
 	$(".project-entry:last").append(formattedName);
-	var formattedDate = HTMLprojectDates.replace("%data%", projects[value].year)
+	var formattedDate = HTMLprojectDates.replace("%data%", projects.project[value].year)
 	$(".project-entry:last").append(formattedDate);
-	var formattedDesc = HTMLprojectDescription.replace("%data%", projects[value].description)
+	var formattedDesc = HTMLprojectDescription.replace("%data%", projects.project[value].description)
 	$(".project-entry:last").append(formattedDesc);
-	var formattedURL = HTMLprojectImage.replace("%data%", projects[value].image)
+	var formattedURL = HTMLprojectImage.replace("%data%", projects.project[value].image)
 	$(".project-entry:last").append(formattedURL);
-
-		}
-
-	}
-}
-
-displaybio();
-displayWork();
-displayEducation();
-displayOnline();
-displayProjects();
+}}}}
+bio.displaybio();
+work.displayWork();
+education.displayEducation();
+education.displayOnline();
+projects.displayProjects();
 $("#main").append(internationalizeButton)
 
 function inName(name)
